5b37f03768007cf08486f37bb4800cc025b892de
JeongGod <jeonggu223@gmail.com>
2022-05-21 14:27:33 +0900

    feat : eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: "lf",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  useTabs: false,
+  arrowParens: 'avoid',
+  printWidth: 120,
+};
