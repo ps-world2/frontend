83398925e5396f58f0911c410139128782df51c6
JeongGod <jeonggu223@gmail.com>
2022-06-12 18:13:59 +0900

    feat : markdown text area 추가

--- src/pages/posts/Post.jsx
+++ src/pages/posts/Post.jsx
@@ -1,4 +1,6 @@
 import { React, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
@@ -9,6 +11,7 @@ export default function Post() {
   };
   return (
     <div>
+      <ReactMarkdown children={inputValue} remarkPlugins={[remarkGfm]} />
       <TextField id="fullWidth" label="글" multiline rows={30} onChange={e => setInputValue(e.target.value)} />
       <Button variant="contained" onClick={handlePost}>
         제출
